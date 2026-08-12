d5c74d42f57724be03d611b34234882767733a17
R. Inocencio <Inocencio@Oakland.edu>
2026-08-12 10:47:17 -0400

    fix CORS test

--- server/Program.cs
+++ server/Program.cs
@@ -2,7 +2,7 @@ using server.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var clientUrl = "https://chess-cheaters-5smdiblgs-inocencio1025s-projects.vercel.app";
+var clientUrl = "https://chess-cheaters-indol.vercel.app";
 
 builder.Services.AddSignalR();
 
