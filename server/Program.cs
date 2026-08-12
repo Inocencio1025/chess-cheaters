f73f4061d5ea450a77cea41c82de4fc383a557fc
R. Inocencio <Inocencio@Oakland.edu>
2026-08-12 10:43:29 -0400

    fix CORS

--- server/Program.cs
+++ server/Program.cs
@@ -2,24 +2,29 @@ using server.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var clientUrl = "https://chess-cheaters-5smdiblgs-inocencio1025s-projects.vercel.app";
+
 builder.Services.AddSignalR();
 
 builder.Services.AddCors(options =>
 {
-  options.AddPolicy("Client", policy =>
-  {
-    policy
-          .WithOrigins("http://localhost:5173")
-          .AllowAnyHeader()
-          .AllowAnyMethod()
-          .AllowCredentials();
-  });
+    options.AddPolicy("Client", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173",
+                clientUrl
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
 });
 
 var app = builder.Build();
 
 app.UseCors("Client");
-app.UseCors("ChessClient");
 
 app.MapHub<GameHub>("/game");
+
 app.Run();
